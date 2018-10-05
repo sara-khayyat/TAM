72638bb90d95c2e29176f813e1c2aea2dc60613f
nenoAls <43853949+nenoAls@users.noreply.github.com>
2018-10-06 00:55:19 +0300

    Add files via upload

---
+++ sideBarActions.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function(){
+	/* hiding  ALL sub Pages*/
+
+		$(".ulHome").hide();
+
+		
+/* showing newProjects ONLY */
+	$(".home").click(function(){
+		$(".ulHome").toggle();
+
+	});
+
+	
+});
+
+
+
+
+	
+
+
+
